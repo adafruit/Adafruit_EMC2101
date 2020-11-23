c5a450a71a875a4cf14c94bb7819db54406b41e1
Doxygen CI <travis@travis-ci.org>
2020-11-23 20:45:08 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['configfanspinup',['configFanSpinup',['../class_adafruit___e_m_c2101.html#ae2c14b4bcd3e300390b7ff29f833506b',1,'Adafruit_EMC2101::configFanSpinup(uint8_t spinup_drive, uint8_t spinup_time)'],['../class_adafruit___e_m_c2101.html#a8776bc1f95c374924153c9f3c88240cc',1,'Adafruit_EMC2101::configFanSpinup(bool tach_spinup)']]]
+  ['configfanspinup',['configFanSpinup',['../class_adafruit___e_m_c2101.html#ae2c14b4bcd3e300390b7ff29f833506b',1,'Adafruit_EMC2101::configFanSpinup(uint8_t spinup_drive, uint8_t spinup_time)'],['../class_adafruit___e_m_c2101.html#a8776bc1f95c374924153c9f3c88240cc',1,'Adafruit_EMC2101::configFanSpinup(bool tach_spinup)']]],
+  ['configpwmclock',['configPWMClock',['../class_adafruit___e_m_c2101.html#a162edee3286c90e0000c6d94560dd2e8',1,'Adafruit_EMC2101']]]
 ];
